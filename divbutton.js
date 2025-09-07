2825ce7e22e4073577f1c6e40478f8dc99d63a80
jacks-lab <jackswarkentin@gmail.com>
2025-09-07 15:48:25 -0700

    EXPAND THE WEBSITE!!!
    Add new files

---
+++ divbutton.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+            const myDivButton = document.getElementById('DivButtonP1');
+            myDivButton.addEventListener('click', function() {
+                window.location.href = "rickrollhomePR.html";
+            });
+            myDivButton.addEventListener('keypress', function(event) {
+                if (event.key === 'Enter' || event.key === ' ') {
+                    window.location.href = "rickrollhomePR.html";
+                }
+            });
+        });
+document.addEventListener('DOMContentLoaded', function() {
+            const myDivButton = document.getElementById('DivButtonP2');
+            myDivButton.addEventListener('click', function() {
+                window.location.href = 'placeholder.html';
+            });
+            myDivButton.addEventListener('keypress', function(event) {
+                if (event.key === 'Enter' || event.key === ' ') {
+                    window.location.href = 'placeholder.html';
+                }
+            });
+        });
+document.addEventListener('DOMContentLoaded', function() {
+            const myDivButton = document.getElementById('DivButtonP3');
+            myDivButton.addEventListener('click', function() {
+                window.location.href = 'placeholder.html';
+            });
+            myDivButton.addEventListener('keypress', function(event) {
+                if (event.key === 'Enter' || event.key === ' ') {
+                    window.location.href = 'placeholder.html';
+                }
+            });
+        });
+document.addEventListener('DOMContentLoaded', function() {
+            const myDivButton = document.getElementById('DivButtonP4');
+            myDivButton.addEventListener('click', function() {
+                window.location.href = 'placeholder.html';
+            });
+            myDivButton.addEventListener('keypress', function(event) {
+                if (event.key === 'Enter' || event.key === ' ') {
+                    window.location.href = 'placeholder.html';
+                }
+            });
+        });
